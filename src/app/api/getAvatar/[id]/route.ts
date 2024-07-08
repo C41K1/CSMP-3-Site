79ad26e8b59a10433c3efdd9225ef2c8b3646652
Caiki <caiki2003@gmail.com>
2024-07-07 23:01:48 -0300

    avatares

---
+++ src/app/api/getAvatar/[id]/route.ts
@@ -0,0 +1,9 @@
+import { NextResponse } from "next/server"
+
+export async function GET({ params }: { params: { id: string } }) {
+
+    if (process.env.ORIGINAIS != undefined && process.env.ORIGINAIS.includes(params.id)) {
+        return NextResponse.json(`https://mc-heads.net/avatar/${params.id}.png`, { status: 200 })
+    }
+    else return NextResponse.json(`https://mc-heads.net/avatar/MHF_steve.png`, { status: 200 })
+}
