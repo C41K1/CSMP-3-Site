18d58fbfa00ed79be080a7713aa0f97850a7af83
Caiki <caiki2003@gmail.com>
2024-07-07 23:25:29 -0300

    redirect

--- src/app/api/getAvatar/[id]/route.ts
+++ src/app/api/getAvatar/[id]/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server"
 export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
 
     if (process.env.ORIGINAIS != undefined && JSON.parse(process.env.ORIGINAIS).includes(params.id)) {
-        return NextResponse.json(`https://mc-heads.net/avatar/${params.id}.png`, { status: 200 })
+        return NextResponse.redirect(`https://mc-heads.net/avatar/${params.id}`)
     }
-    else return NextResponse.json(`https://mc-heads.net/avatar/MHF_steve.png`, { status: 200 })
+    else return NextResponse.redirect(`https://mc-heads.net/avatar/MHF_steve`)
 }
