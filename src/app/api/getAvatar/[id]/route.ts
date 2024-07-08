9ab4f6008cdfe9cf2da33b90619776ce5df771ff
Caiki <caiki2003@gmail.com>
2024-07-07 23:32:22 -0300

    customizadas

--- src/app/api/getAvatar/[id]/route.ts
+++ src/app/api/getAvatar/[id]/route.ts
@@ -2,7 +2,11 @@ import { NextRequest, NextResponse } from "next/server"
 
 export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
 
-    if (process.env.ORIGINAIS != undefined && JSON.parse(process.env.ORIGINAIS).includes(params.id)) {
+    if (process.env.CUSTOMIZADAS != undefined && JSON.parse(process.env.CUSTOMIZADAS).nick.includes(params.id)) {
+        let avatar = JSON.parse(process.env.CUSTOMIZADAS).avatar[JSON.parse(process.env.CUSTOMIZADAS).nick.indexOf(params.id)]
+        return NextResponse.redirect(`https://mc-heads.net/avatar/${avatar}`)
+    }
+    else if (process.env.ORIGINAIS != undefined && JSON.parse(process.env.ORIGINAIS).includes(params.id)) {
         return NextResponse.redirect(`https://mc-heads.net/avatar/${params.id}`)
     }
     else return NextResponse.redirect(`https://mc-heads.net/avatar/MHF_steve`)
