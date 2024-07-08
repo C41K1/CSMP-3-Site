20f58306a6c55c142e3290b6da0e88137ec628d6
Caiki <caiki2003@gmail.com>
2024-07-07 23:03:35 -0300

    agr vai

--- src/app/api/getAvatar/[id]/route.ts
+++ src/app/api/getAvatar/[id]/route.ts
@@ -1,6 +1,6 @@
-import { NextResponse } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 
-export async function GET({ params }: { params: { id: string } }) {
+export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
 
     if (process.env.ORIGINAIS != undefined && process.env.ORIGINAIS.includes(params.id)) {
         return NextResponse.json(`https://mc-heads.net/avatar/${params.id}.png`, { status: 200 })
